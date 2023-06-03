2f945c1a652825624ade9bc41bc45e7dda5b5365
AnnaToimurzina <2402as@gmail,com>
2023-06-03 14:02:56 +0500

    файл Phone работает

--- src/phone.py
+++ src/phone.py
@@ -3,16 +3,16 @@ from src.item import Item
 class Phone(Item):
     '''Cоздание экземпляра класса Phone
     # Переопределяем метод базового класса'''
-    def init(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
+    def __init__(self, name: str, price: float, quantity: int, _number_of_sim: int) -> None:
         '''# Вызываем метод базового класса'''
         super().__init__(name, price, quantity)
         '''# Дополнительный код'''
-        self.number_of_sim = number_of_sim
+        self.number_of_sim = _number_of_sim
 
     @property
     def number_of_sim(self):
         """Возвращает атрибут, содержащий количество поддерживаемых сим-карт. К атрибуту можно обращаться без ()."""
-        return self.number_of_sim
+        return self._number_of_sim
 
     @number_of_sim.setter
     def number_of_sim(self, sim_number: int):
@@ -25,7 +25,7 @@ class Phone(Item):
         """
         Возвращает строку с описанием объекта Phone.
         """
-        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity}, {self._number_of_sim})"
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self._number_of_sim})"
 
 
 
